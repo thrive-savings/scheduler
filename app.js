7b9ad0763db17329a353e89a099fed84ad83c401
nbaghiro <naib.baghirov@gmail.com>
2019-03-25 15:56:52 -0400

    fully rewritten

--- app.js
+++ app.js
@@ -7,110 +7,76 @@ const { API, API_SECRET } = require('./config')
 const app = express()
 
 try {
-  const FETCH_FREQUENCIES = [
-    // Money pulling
-    'ONCEWEEKLY',
-    'TWICEWEEKLY',
-    'BIWEEKLY',
-    'ONCEMONTHLY',
-    'ONCEDAILY',
-    // Other tasks
-    'ASK_ALGO_BOOST',
-    'FIRE_NOTIFICATIONS',
-    'FETCH_DAILY_UPDATES',
-    'MONTHLY_STATEMENT'
-  ]
+  const JOBS = {
+    SAVER: {
+      endpoint: 'saver-run'
+    },
+    FETCHER: {
+      endpoint: 'quovo-fetch-updates'
+    },
+    NOTIFIER: {
+      endpoint: 'notifications-fire'
+    },
 
-  const convertFrequency = frequency => {
+    ASK_ALGO_BOOST: {
+      endpoint: 'notifications-ask-boost'
+    },
+    MONTHLY_STATEMENT: {
+      endpoint: 'notifications-monthly-statement'
+    }
+  }
+
+  const getSchedulerRule = frequency => {
     const rule = new scheduler.RecurrenceRule()
     rule.tz = 'UTC'
     rule.hour = 14
     rule.minute = 0
     switch (frequency) {
-      case 'ONCEWEEKLY':
-        rule.dayOfWeek = 1
+      default:
+      case 'SAVER':
         break
-      case 'TWICEWEEKLY':
-        rule.dayOfWeek = [1, 3]
+      case 'NOTIFIER':
+        rule.hour = 15
         break
-      case 'BIWEEKLY':
-        rule.date = [1, 15]
+      case 'FETCHER':
+        rule.hour = 10
         break
-      case 'ONCEMONTHLY':
-        rule.date = 1
-        break
-      case 'ONCEDAILY':
-        break
-      case 'EVERYHOUR':
-        rule.hour = null
-        break
-      case 'EVERYMINUTE':
-        rule.hour = null
-        rule.minute = new scheduler.Range(0, 59, 1)
-        break
+
       case 'ASK_ALGO_BOOST':
         rule.date = 15
         rule.hour = 16
-        break
-      case 'FIRE_NOTIFICATIONS':
-        rule.hour = 15
-        break
-      case 'FETCH_DAILY_UPDATES':
-        rule.hour = 10
         break
       case 'MONTHLY_STATEMENT':
         rule.date = 1
         rule.hour = 16
         break
-      default:
-        // ONCEWEEKLY
-        rule.dayOfWeek = 1
-        break
     }
     return rule
   }
 
-  const fire = async frequencyWord => {
+  const fire = job => {
     const now = new Date(Date.now())
-    console.log(
-      `Scheduler running for frequency ${frequencyWord} at ${now.toString()}`
-    )
+    console.log(`Scheduler running for the job ${job} at ${now.toString()}`)
 
     const apiSecret = { secret: API_SECRET }
 
-    let command = 'worker-send-boost-notification'
-    let commandBody = { ...apiSecret }
-
-    if (frequencyWord === 'FIRE_NOTIFICATIONS') {
-      command = 'notifications-fire'
-    } else if (frequencyWord === 'FETCH_DAILY_UPDATES') {
-      command = 'worker-fetch-daily-updates'
-    } else if (frequencyWord === 'MONTHLY_STATEMENT') {
-      command = 'worker-monthly-statement'
-    } else if (frequencyWord !== 'ASK_ALGO_BOOST') {
-      command = 'worker-run-frequency'
-      commandBody.data = { frequencyWord }
-    }
-
-    await request.post({
-      uri: `${API}/admin/${command}`,
-      body: commandBody,
+    request.post({
+      uri: `${API}/admin/${JOBS[job].endpoint}`,
+      body: { ...apiSecret },
       json: true
     })
 
     request.post({
-      uri: `${API}/admin/worker-notify-about-scheduler-run`,
-      body: { ...apiSecret, data: { task: command } },
+      uri: `${API}/admin/manual-notify-about-scheduler-run`,
+      body: { ...apiSecret, data: { job } },
       json: true
     })
   }
 
   // Schedule jobs
-  FETCH_FREQUENCIES.map(async frequencyWord => {
-    const frequency = convertFrequency(frequencyWord)
-    scheduler.scheduleJob(frequency, async () => {
-      await fire(frequencyWord)
-    })
+  Object.keys(JOBS).map(job => {
+    const rule = getSchedulerRule(job)
+    scheduler.scheduleJob(rule, () => fire(job))
   })
 } catch (error) {
   if (error.stack) console.log(error.stack)
